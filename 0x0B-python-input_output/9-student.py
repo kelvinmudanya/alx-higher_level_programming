ca73ae8b37d8b31840ee931ab2f971ac3101a715
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2023-01-10 16:28:18 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-""" script that adds arguments to a python list and saven them """
-import os.path
-from sys import argv
-""" sys allows me to receive the string to add in the file from the cl
-for accessing the filesystem os.path """
+""" class Student """
 
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+class Student:
+    """ definition """
+    def __init__(self, first_name, last_name, age):
+        """ instantiation with
+        Args:
+            first_name (str): public instance attributes
+            last_name (str): public instance attributes
+            age (int): public instance attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+    def to_json(self, attrs=None):
+        """ public method that retrieves a dictionary representation
+        of a student instance """
+        if isinstance(attrs, list) and ([isinstance(n, str)] for n in attrs):
+            new_dictionary = {}
+            for words in attrs:
+                if words in self.__dict__:
+                    new_dictionary[words] = self.__dict__[words]
+            return new_dictionary
 
-p_list = []
-if os.path.exists("add_item.json"):
-    p_list = load_from_json_file("add_item.json")
-save_to_json_file(p_list + argv[1:], "add_item.json")
+        return self.__dict__
