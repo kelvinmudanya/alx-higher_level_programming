8dce94370c8eea34a042209360b1bd7816c84ee1
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2023-01-10 16:00:28 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" method to check if is instance """
+""" method to check inheritance """
 
 
-def is_kind_of_class(obj, a_class):
-    """ method definition """
-    return isinstance(obj, a_class)
+def inherits_from(obj, a_class):
+    """ method defitinion returns True or False """
+    return isinstance(obj, a_class) and type(obj) is not a_class
