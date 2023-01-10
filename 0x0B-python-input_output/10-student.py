e9da4a72fc01127c885368987495b0e84c3eacf3
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2023-01-10 16:06:06 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-""" JSON serialization of an object """
+""" class Student """
 
 
-def class_to_json(obj):
-    """  returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON serialization
-    of an object """
-    return obj.__dict__
+class Student:
+    """ definition """
+    def __init__(self, first_name, last_name, age):
+        """ instantiation with
+        Args:
+            first_name (str): public instance attributes
+            last_name (str): public instance attributes
+            age (int): public instance attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ public method that retrieves a dictionary representation
+        of a student instance """
+        return self.__dict__
