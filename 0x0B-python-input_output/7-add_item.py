8084124c45602dd793575b82becbe3a5e71bbfb2
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2023-01-10 16:30:05 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" json method that writes an object to a text file """
-import json
+""" script that adds arguments to a python list and saven them """
+import os.path
+from sys import argv
+""" sys allows me to receive the string to add in the file from the cl
+for accessing the filesystem os.path """
 
 
-def save_to_json_file(my_obj, filename):
-    """ json method definition
-    Args:
-        my_obj (str): to write
-        filename (str): file to write in using json representation
-    """
-    with open(filename, encoding='utf-8', mode='w') as file:
-        json.dump(my_obj, file)
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+p_list = []
+if os.path.exists("add_item.json"):
+    p_list = load_from_json_file("add_item.json")
+save_to_json_file(p_list + argv[1:], "add_item.json")
