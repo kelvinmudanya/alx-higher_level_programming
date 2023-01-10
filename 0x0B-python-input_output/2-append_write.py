57c00386c6cd4e46c8fb0ac31e4ddd3326d3b3e7
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2023-01-10 16:27:30 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-""" method that reads n lines of a file """
+""" method that appends a string at the end of a text file """
 
 
-def read_lines(filename="", nb_lines=0):
-    """ read_lines definition
+def append_write(filename="", text=""):
+    """ method append_write definition
     Args:
-        filename (str): the passed file
-        nb_lines (int): the amount of lines to read
+        filename (str): the passed file to read and append to
+        text (str): the string to append on the file
     """
-    with open(filename, encoding='utf-8') as file:
-        if nb_lines <= 0:
-            print(file.read(), end='')  # the complete file
-        for line_num, line in enumerate(file):
-            if line_num >= nb_lines:
-                break  # prints the n lines
-            print(line, end='')
+    with open(filename, encoding='utf-8', mode='a') as file:
+        return file.write(text)
